6c4d83ff14c5793dbff4fa8bb5a843fea2889d91
Rajarajan <ksrajarajan13@gmail.com>
2025-09-20 11:43:27 +0530

    Delete rmps.sh

--- rmps.sh
+++
@@ -1,9 +0,0 @@
-#bin/bash 
-docker stop schedule_container
-docker rm schedule_container
-
-docker stop frontend_container
-docker rm frontend_container
-
-docker stop auth_container
-docker rm auth_container
